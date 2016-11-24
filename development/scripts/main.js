655bba529ad9d51232f7b450e07535f633479fed
Arman Yessymkanov <arman.yessymkanov@gmail.com>
2016-11-25 00:19:54 +0600

    Implemented Hero section image slider

--- development/scripts/main.js
+++ development/scripts/main.js
@@ -1,9 +1,19 @@
 (function() {
   var slides = document.querySelectorAll('.slides__item');
-  var count = 1;
+  var slidesNav = document.querySelectorAll('.slides__navigation__item');
+  var count = 0, current = count;
+  console.log(slidesNav);
+
   var slider = setInterval(function() {
 
-    
+    slides[count].classList.toggle('active');
+    slidesNav[count].classList.toggle('current');
+    if(count === slides.length - 1) {
+      count = -1;
+    }
+    slides[++count].classList.toggle('active');
+    slidesNav[count].classList.toggle('current');
+
     console.log(count);
   }, 3000);
 })();
